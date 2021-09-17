e1c7d6e44b0e99d4c9f8cbad01662a92b209f645
KSH12181747 <90024364+KSH12181747@users.noreply.github.com>
2021-09-17 16:21:17 +0900

    Add files via upload

---
+++ Ex4-4_Driver.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include "Ex4-4_Header.h"
+
+int main() {
+	GradeBook gradeBook1("cs101 introduction to C++ Programming");
+	GradeBook gradeBook2("cs102 Data Structures in C++");
+
+	cout << "gradeBook1 creates for course: " << gradeBook1.getCourseName()
+	<< "\ngradeBook2 creates for course: " << gradeBook2.getCourseName()
+	<< endl;
+	
+	return 0;
+}
